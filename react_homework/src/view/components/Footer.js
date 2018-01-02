bd4ed337255d4d3b3742ea9c6aeba0401885868c
estrella-kim <puris9030@gmail.com>
2018-01-02 10:33:25 +0900

    react initial commit 수정

---
+++ react_homework/src/view/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../css/footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            푸터 영역입니다.
+        </div>
+    )
+}
+
+export default Footer;
