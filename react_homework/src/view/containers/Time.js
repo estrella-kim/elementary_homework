3153293f09d9ec9268755793950c92fcb51d9d76
EstrellaJYKIM <puris9030@gmail.com>
2018-01-07 13:25:44 +0900

    코멘트 추가 및 수정

--- react_homework/src/view/containers/Time.js
+++ react_homework/src/view/containers/Time.js
@@ -4,17 +4,19 @@ import '../../css/watch.css';
 class Time extends React.Component{
     constructor() {
         super();
-        this.state = this.setTime();
+        this.state = Time.setTime();
+        this.continue = null; //뜬금없이 등장하면 당황스러우니 초기화해준다.
     }
-    setTime () {
+    //this 가 찍는건 time 을 객체로 반환해주는데, {굳이 한 과정을 추가하는 것보다ㅏ} time파일에서 setTime을 선언.
+    static setTime () {
         return{
             presentTime : new Date().toLocaleTimeString(),
-            color : 'rgb('+ Math.ceil(Math.random() * 257) +',' + Math.ceil(Math.random() * 257) +','+ Math.ceil(Math.random() * 257)+')'
+            color : 'rgb('+ Math.ceil(Math.random() * 257) +',' + Math.ceil(Math.random() * 257) +','+ Math.ceil(Math.random() * 257)+')' //따로 빼기
         }
     }
     beginTime() {
         this.continue = setInterval( () =>{
-            this.setState((state) => this.setTime())
+            this.setState(Time.setTime()); //react는 새로운 객체가 계속 생성된다..  10분동안 한다면 600개의 관계없는 객체가 생성된다. 만일 더이상 react에서 더이상 참조하지 않으면 브라우저의 가비지 컬렉ㅌ터가 해당 객체를 삭제해줄것이다.
         }, 1000)
     }
     componentDidMount(){
@@ -22,9 +24,10 @@ class Time extends React.Component{
     }
 
     watch() {
+        // state인자는 쓰일 때 넣어주는 게
         this.setState((state) => {
             return{
-                play : !this.state.play
+                play : !state.play
             }
         })
         if(this.state.play){
