3504e31dd8f24e1265071c522b750f95332d86b5
estrella-kim <puris9030@gmail.com>
2018-01-10 17:15:16 +0900

    util 정리방법  2

--- react_homework/src/util/random_util/randomColor.js
+++ react_homework/src/util/random_util/randomColor.js
@@ -1,6 +1,5 @@
-var randomNum = require('./randomNum');
-
-module.exports= function randomColor () {
+import randomNum from './randomNum';
+export default function randomColor () {
     var randomColor = 'rgb('+ randomNum(0, 255) +',' + randomNum(0, 255) +','+ randomNum(0, 255) +')';
     return randomColor;
 }
