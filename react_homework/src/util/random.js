6aeeb2c2f4eb78d0d237c9d723d1318d492f5346
EstrellaJYKIM <puris9030@gmail.com>
2018-01-14 16:53:55 +0900

    날아간 코드 복구

--- react_homework/src/util/random.js
+++ react_homework/src/util/random.js
@@ -1,7 +1,7 @@
 export default {
     color() {
         //eslint-disable-next-line
-        let color = 'rgb('+ this.number(0, 255) +',' + this.number(0, 255) +','+ this.number(0, 255) +')';
+        let color = `rgb${this.number(0, 255)},${this.number(0, 255)},${this.number(0, 255)})`;
         return color;
     },
     // a 와 같거나 큰 수 부터 b보다 같거나 작은 수 사이의 랜덤한 정수를 구한다.
